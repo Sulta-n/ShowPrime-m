4b44b4a4110d95a4264eae36557f26c976d5a2b5
Aleem <meliodaslord280@gmail.com>
2025-06-29 18:38:39 +0100

    remove commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import Navbar from "../components/Navbar";
 import HomeSection from "../components/HomeSection";
 import GetStarted from "../components/GetStarted";
 import Features from "../components/Features";
-import Community from "../components/community";
 
 const Home = () => {
   return (
